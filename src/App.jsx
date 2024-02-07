8599a3f19740ddc3be9894ed7e22bf5eb16e9ade
devroy10 <roychinwuba@gmail.com>
2024-02-07 06:57:05 +0100

    fixed bug of storing new tasks

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,12 @@ const FILTER_MAP = {
 
 const FILTER_NAMES = Object.keys(FILTER_MAP);
 
-
 function App() {
-
   const [tasks, setTasks] = useState([]);
+  const [filter, setFilter] = useState("All");
 
   const getTasks = () => {
-    const tasksFromLocalStorage = localStorage.getItem('tasks');
+    const tasksFromLocalStorage = localStorage.getItem("tasks");
     const data = tasksFromLocalStorage ? JSON.parse(tasksFromLocalStorage) : [];
     setTasks(data);
   };
@@ -29,9 +28,6 @@ function App() {
   }, []);
 
 
-  const [filter, setFilter] = useState("All");
-
-
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map((task) => {
       // if this task has the same ID as the edited task
@@ -39,9 +35,8 @@ function App() {
         // use object spread to make a new object
         // whose `completed` prop has been inverted
         return { ...task, completed: !task.completed };
-
       }
-      console.log(tasks)
+      console.log(tasks);
       return task;
     });
     setTasks(updatedTasks);
@@ -53,21 +48,23 @@ function App() {
   function deleteTask(id) {
     const remainingTasks = tasks.filter((task) => id !== task.id);
     setTasks(remainingTasks);
-    localStorage.setItem('tasks', JSON.stringify(remainingTasks));
-    sessionStorage.setItem('tasks')
+    localStorage.setItem("tasks", JSON.stringify(remainingTasks));
+    sessionStorage.setItem("tasks");
   }
 
-  const taskList = tasks.filter(FILTER_MAP[filter]).map((task) => (
-    <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
-      editTask={editTask}
-    />
-  ));
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
 
   const filterList = FILTER_NAMES.map((name) => (
     <FilterButton
@@ -75,12 +72,13 @@ function App() {
       name={name}
       isPressed={name === filter}
       setFilter={setFilter}
-    />));
+    />
+  ));
 
   function addTask(name) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false };
     setTasks([...tasks, newTask]);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem("tasks", JSON.stringify([...tasks, newTask]));
   }
 
   function editTask(id, newName) {
@@ -97,20 +95,18 @@ function App() {
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
   const headingText = `${taskList.length} ${tasksNoun}`;
 
-
   return (
     <div>
       <div className="todoapp stack-large">
         <h1>Simple Todo App</h1>
         <TaskForm addTask={addTask} />
-        <div className="filters btn-group stack-exception">
-          {filterList}
-        </div>
+        <div className="filters btn-group stack-exception">{filterList}</div>
         <h2 id="list-heading">{headingText}</h2>
         <ul
           role="list"
           className="todo-list stack-large stack-exception"
-          aria-labelledby="list-heading">
+          aria-labelledby="list-heading"
+        >
           {taskList}
         </ul>
       </div>
